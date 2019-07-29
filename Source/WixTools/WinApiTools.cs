e0006e62085cf4a9ac0b85c278f90d8a73954401
BeePerNet <sebasgariepy@gmail.com>
2019-07-29 19:04:10 -0400

    CustomAction for language working. Testing...

---
+++ Source/WixTools/WinApiTools.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace WixTools
+{
+    internal static class WinApiTools
+    {
+
+
+        public static IEnumerable<CultureInfo> GetInstalledInputLanguages()
+        {
+            // first determine the number of installed languages
+            uint size = GetKeyboardLayoutList(0, null);
+            IntPtr[] ids = new IntPtr[size];
+
+            // then get the handles list of those languages
+            GetKeyboardLayoutList(ids.Length, ids);
+
+            foreach (int id in ids) // note the explicit cast IntPtr -> int
+            {
+                yield return new CultureInfo(id & 0xFFFF);
+            }
+        }
+               
+
+        [DllImport("user32.dll")]
+        private static extern uint GetKeyboardLayoutList(int nBuff, [Out] IntPtr[] lpList);
+    }
+}
---
+++ Source/WixTools/WixActions.cs
@@ -0,0 +1,27 @@
+using Microsoft.Deployment.WindowsInstaller;
+using System;
+using System.Linq;
+
+namespace WixTools
+{
+    public static class WixActions
+    {
+        [CustomAction]
+        public static ActionResult GetLanguages(Session session)
+        {
+            try
+            {
+                string WinInstalledLanguages = string.Join(",", WinApiTools.GetInstalledInputLanguages().Select(T => T.TwoLetterISOLanguageName).Distinct().ToArray());
+
+                session.Log("WinInstalledLanguages {0}", WinInstalledLanguages);
+                session["WinInstalledLanguages"] = WinInstalledLanguages;
+            }
+            catch (Exception ex)
+            {
+                session.Log("ERROR in custom action GetLanguages {0}", ex.ToString());
+                return ActionResult.Failure;
+            }
+            return ActionResult.Success;
+        }
+    }
+}
