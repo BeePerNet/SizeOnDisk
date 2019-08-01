4683bbd41218b4d5e30426af2b8692a5c54478d4
BeePerNet <sebasgariepy@gmail.com>
2019-08-01 14:28:23 -0400

    Threading

--- Source/SizeOnDisk/Properties/AssemblyInfo.cs
+++ Source/SizeOnDisk/Properties/AssemblyInfo.cs
@@ -57,5 +57,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.10.42")]
-[assembly: AssemblyFileVersion("2.4.10.42")]
+[assembly: AssemblyVersion("2.5.0.42")]
+[assembly: AssemblyFileVersion("2.5.0.42")]
