9053c10495fe87f8492b31b7923eeb85bd45507c
BeePerNet <sebasgariepy@gmail.com>
2019-08-20 01:21:57 -0400

    Replaced TextBox by TextBlock with copy

---
+++ SizeOnDisk/Themes/TextBlock.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SizeOnDisk.Themes
+{
+    public partial class TextBlock : ResourceDictionary
+    {
+        public void CallCopyCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (sender is ContextMenu ctx && ctx.PlacementTarget is System.Windows.Controls.TextBlock tb)
+            {
+                Clipboard.SetText(tb.Text);
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(sender));
+            }
+        }
+
+    }
+}
