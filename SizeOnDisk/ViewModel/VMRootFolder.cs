65673ab8000ce098172e924d8b266790ba083643
BeePerNet <sebasgariepy@gmail.com>
2019-08-23 09:57:41 -0400

    Using cleanup

--- SizeOnDisk/ViewModel/VMRootFolder.cs
+++ SizeOnDisk/ViewModel/VMRootFolder.cs
@@ -1,7 +1,6 @@
 ﻿using SizeOnDisk.Shell;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
